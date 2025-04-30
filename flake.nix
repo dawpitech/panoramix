50de7d5ab781d986640c0d774fcea369dd3da07a
Antoine <me@antoinebellanger.fr>
2025-04-30 11:37:00 +0200

    Initial commit
    Signed-off-by: Antoine <me@antoinebellanger.fr>

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell.override {stdenv = pkgs.multiStdenv;} {
+          packages = with pkgs; [
+            gnumake
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
